9baded23d37cc8fac8be101ebe912a7d69225336
MEGABOOK <MEGABOOK@MEGABOOK>
2024-02-07 10:32:33 +0700

    Добавьте файлы проекта.

---
+++ Less1_razr/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Less1_razr.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Short() 
+        { 
+            return View();
+        }
+    }
+}
